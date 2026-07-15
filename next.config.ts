8186275fc4cda377786c1691ae78d398a9bcebe2
mugisham37 <mugisham333@gmail.com>
2026-07-15 10:24:10 +0200

    chore: simplify next.config to defaults

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  typedRoutes: true,
-  experimental: {
-    turbopackFileSystemCacheForBuild: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
