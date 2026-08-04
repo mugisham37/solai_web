639abe697c64d727485b779acae6e073f89af7ba
mugisham37 <mugisham333@gmail.com>
2026-08-04 08:52:26 +0200

    chore(dashboard): remove settings route layout

--- src/app/(dashboard)/settings/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SettingsLayout } from "@/components/organisms/app/settings/SettingsLayout";
-
-export default function SettingsRootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SettingsLayout>{children}</SettingsLayout>;
-}
